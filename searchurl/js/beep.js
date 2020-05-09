5a4ac9a6f5a9256c1b8be23e49483ea0c1bcbded
James Feng Cao <jamesfengcao@yahoo.com>
2020-05-09 22:42:57 +0800

    rebuilding site Sat 09 May 2020 10:42:57 PM CST

---
+++ searchurl/js/beep.js
@@ -0,0 +1,19 @@
+//data:charset=utf-8,
+var audioCtx = new window.AudioContext;
+function beep(duration, frequency, volume, type, callback) {
+    var oscillator = audioCtx.createOscillator();
+    var gainNode = audioCtx.createGain();
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+
+    if (volume){gainNode.gain.value = volume;}
+    if (frequency){oscillator.frequency.value = frequency;}
+    if (type){oscillator.type = type;}
+    if (callback){oscillator.onended = callback;}
+
+    oscillator.start(audioCtx.currentTime);
+    oscillator.stop(audioCtx.currentTime + ((duration || 500) / 1000));
+};
+beep();
+self.close();
