c29d189ab96e5bdadfb1f7f166a5703b4c9995cb
James Feng Cao <jamesfengcao@yahoo.com>
2020-08-24 10:55:05 +0800

    rebuilding site Mon Aug 24 10:55:04 AM CST 2020

--- searchurl/script/filecap_microsoft.sh
+++ searchurl/script/filecap_microsoft.sh
@@ -4,11 +4,11 @@ mp3:text/html:echo '<audio controls width=100% height=100%><source src="%u"></au
 m4b:text/html:echo '<audio controls width=100% height=100%><source src="%u"></audio>'
 mp4:text/html:echo '<video controls width=100% height=100%><source src="%u"></video>'
 mkv:text/html:echo '<video controls width=100% height=100%><source src="%u"></video>'
-doc::am start --user 0 -a android.intent.action.VIEW -d 'https://view.officeapps.live.com/op/view.aspx?src=%U'
-xls::am start --user 0 -a android.intent.action.VIEW -d 'https://view.officeapps.live.com/op/view.aspx?src=%U'
-ppt::am start --user 0 -a android.intent.action.VIEW -d 'https://view.officeapps.live.com/op/view.aspx?src=%U'
-docx::am start --user 0 -a android.intent.action.VIEW -d 'https://view.officeapps.live.com/op/view.aspx?src=%U'
-xlsx::am start --user 0 -a android.intent.action.VIEW -d 'https://view.officeapps.live.com/op/view.aspx?src=%U'
-pptx::am start --user 0 -a android.intent.action.VIEW -d 'https://view.officeapps.live.com/op/view.aspx?src=%U'
+doc:uweb:echo https://view.officeapps.live.com/op/view.aspx?src=%U
+xls:uweb:echo https://view.officeapps.live.com/op/view.aspx?src=%U
+ppt:uweb:echo https://view.officeapps.live.com/op/view.aspx?src=%U
+docx:uweb:echo https://view.officeapps.live.com/op/view.aspx?src=%U
+xlsx:uweb:echo https://view.officeapps.live.com/op/view.aspx?src=%U
+pptx:uweb:echo https://view.officeapps.live.com/op/view.aspx?src=%U
 EOF
 
