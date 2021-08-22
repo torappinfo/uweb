66849cadbbef0e9161ea67ea844b1a4ab1688f2f
James Feng Cao <jamesfengcao@torapp.info>
2021-08-22 11:29:31 +0800

    rebuilding site Sun Aug 22 11:29:31 AM CST 2021

--- searchurl/config.js
+++ searchurl/config.js
@@ -5,7 +5,7 @@ if(d.body)
 
 let labels = ["Default font"];
 let names = ["font"];
-let sa = [`<script>function g(){let f=d.forms[0];d.location.href='i:0i/sdcard/uweb/config.html:'+f.font.value+':http://cdn.jsdelivr.net/gh/torappinfo/uwebzh/searchurl/template/config.html';}<\/script><style>input{height:40px;width:100%;border-radius:50px;}</style><form onsubmit='return g();'>`];
+let sa = [`<script>function g(){let d=document;let f=d.forms[0];d.location.href='i:0i/sdcard/uweb/config.html:'+f.font.value+':http://cdn.jsdelivr.net/gh/torappinfo/uwebzh/searchurl/template/config.html';}<\/script><style>input{height:40px;width:100%;border-radius:50px;}</style><form onsubmit='return g();'>`];
 
 
 let strs = text.split('\n');
