1710fc6c75c91243939216af437c6af674cf2b26
James Feng Cao <jamesfengcao@torapp.info>
2021-08-22 11:21:57 +0800

    rebuilding site Sun Aug 22 11:21:57 AM CST 2021

---
+++ searchurl/config.js
@@ -0,0 +1,17 @@
+let text = "";
+let d=document;
+if(d.body)
+  text = d.body.innerHTML;
+
+let labels = ["Default font"];
+let names = ["font"];
+let sa = [`<script>function g(){let f=d.forms[0];d.location.href='i:0i/sdcard/uweb/config.html:'+f.font.value+':http://cdn.jsdelivr.net/gh/torappinfo/uwebzh/searchurl/template/config.html';}<\/script><style>input{height:40px;width:100%;border-radius:50px;}</style><form onsubmit='return g();'>`];
+
+
+let strs = text.split('\n');
+for (let i = 0; i < labels.length; i++){
+  sa.push("<label>"+labels[i]+"<input type='text' name='"+names[i]+"' value='"+strs[i]+"'/></label>");
+}
+sa.push('<input type="submit" value="Save"></form>');
+let r = sa.join('<br>');
+d.documentElement.innerHTML = r;
