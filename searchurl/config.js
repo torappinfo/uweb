96a50080a7a1e3c44fa7ec70fd8d9d91b2b0c619
James Feng Cao <jamesfengcao@torapp.info>
2021-08-23 16:06:03 +0800

    rebuilding site Mon Aug 23 04:06:03 PM CST 2021

--- searchurl/config.js
+++ searchurl/config.js
@@ -3,9 +3,15 @@ let d=document;
 if(d.body)
   text = d.body.innerHTML;
 
-let labels = ["Default font"];
-let names = ["font"];
-let sa = [`<style>input{height:40px;width:100%;border-radius:50px;}</style><form onsubmit="let d=document;let f=d.forms[0];d.location.href='i:0i/sdcard/uweb/config.html:'+f.font.value+':https://cdn.jsdelivr.net/gh/torappinfo/uwebzh/searchurl/template/config.html';return false;" accept-charset=utf-8 >`];
+let labels = ["网页默认字体族","cursive字体族","Fantasy字体族","Fixed字体族","Sans-Serif字体族","Serif字体族"];
+let names = ["font","cursive","fantasy","fixed","sansserif","serif"];
+let sa = [`<style>input{height:40px;width:100%;border-radius:50px;}</style><form onsubmit="let d=document;let f=d.forms[0];d.location.href='i:0i/sdcard/uweb/config.html:'+f.font.value+
++'&'+ f.cursive.value +
++'&'+ f.fantasy.value +
++'&'+ f.fixed.value +
++'&'+ f.sansserif.value +
++'&'+ f.serif.value +
+':https://cdn.jsdelivr.net/gh/torappinfo/uwebzh/searchurl/template/config.html';return false;" accept-charset=utf-8 >`];
 
 
 let strs = text.split('\n');
