b706b0497a98570dfaa53273c3b18c32d97e5949
James Feng Cao <jamesfengcao@torapp.info>
2021-08-29 07:49:58 +0800

    rebuilding site Sun Aug 29 07:49:58 AM CST 2021

--- searchurl/script/axel.sh
+++ searchurl/script/axel.sh
@@ -2,6 +2,6 @@ apt update
 apt upgrade
 apt install axel
 (cat /sdcard/uweb/default.longclick;cat<<HERE )|awk '!s[$0]++'>a.tmp;mv a.tmp /sdcard/uweb/default.longclick
-axel:cd /sdcard/Download;axel -n 9 
+axel::cd /sdcard/Download;axel -n 9 
 HERE
 
