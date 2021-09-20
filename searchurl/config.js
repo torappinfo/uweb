04fcd354f9966f2b7652d4d39da716b60f530760
James Feng Cao <jamesfengcao@torapp.info>
2021-09-20 20:00:28 +0800

    rebuilding site Mon Sep 20 08:00:27 PM CST 2021

--- searchurl/config.js
+++ searchurl/config.js
@@ -1,12 +1,24 @@
 let labels = ["网页默认字体族","cursive字体族","Fantasy字体族","Fixed字体族","Sans-Serif字体族","Serif字体族"];
 let names = ["font","cursive","fantasy","fixed","sansserif","serif"];
 
-let text = "\n".repeat(labels.length-1);
+let text;
 let d=document;
 if(d.body)
   text = d.body.innerHTML;
+else
+  text = "\n".repeat(labels.length-1);
 
-let sa = [`<style>input{height:40px;width:100%;border-radius:50px;}</style><form onsubmit="let d=document;let f=d.forms[0];d.location.href='i:0i/sdcard/uweb/config.html:'+f.font.value+
+let sa = [`<style>input{height:40px;width:100%;border-radius:50px;}</style>
+<datalist id="family">
+<option value="sans-serif">
+<option value="sans-serif-condensed">
+<option value="sans-serif-smallcaps">
+<option value="serif">
+<option value="serif-monospace">
+<option value="monospace">
+<option value="cursive">
+</datalist>
+<form onsubmit="let d=document;let f=d.forms[0];d.location.href='i:0i/sdcard/uweb/config.html:'+f.font.value+
 '&'+ f.cursive.value +
 '&'+ f.fantasy.value +
 '&'+ f.fixed.value +
@@ -17,7 +29,7 @@ let sa = [`<style>input{height:40px;width:100%;border-radius:50px;}</style><form
 
 let strs = text.split('\n');
 for (let i = 0; i < labels.length; i++){
-  sa.push("<label>"+labels[i]+"<input type='text' name='"+names[i]+"' value='"+strs[i]+"'/></label>");
+  sa.push("<label>"+labels[i]+"<input list='family' type='text' name='"+names[i]+"' value='"+strs[i]+"'/></label>");
 }
 sa.push('<input type="submit" value="Save"></form>');
 let r = sa.join('<br>');
