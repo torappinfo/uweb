ff657106bfbb09c71c4f416f196d9278ef57ffaa
James Feng Cao <jamesfengcao@torapp.info>
2021-10-19 22:23:37 +0800

    rebuilding site Tue Oct 19 10:23:36 PM CST 2021

--- searchurl/script/xzgrep.sh
+++ searchurl/script/xzgrep.sh
@@ -1,4 +1,4 @@
 apt install xz-utils grep less
 cd /sdcard/uweb/dict
-(cat /sdcard/uweb/home.search;ls *.xz|awk -F: '{print $0 "G:c:xz -cdfq /sdcard/uweb/dict/" $0,"|grep -i \\\'%s\\\'|less" }')|awk -F: '!s[$1]++'>a.tmp;mv a.tmp /sdcard/uweb/home.search
+(cat /sdcard/uweb/home5.search;ls *.xz|awk -F: '{print $0 "G:c:xz -cdfq /sdcard/uweb/dict/" $0,"|grep -i \\\'%s\\\'|less" }')|awk -F: '!s[$1]++'>a.tmp;mv a.tmp /sdcard/uweb/home5.search
 
