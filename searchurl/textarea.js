c2044dfba08461dc25076e87f4c460292fa26607
James Feng Cao <jamesfengcao@torapp.info>
2022-04-06 17:13:22 +0800

    rebuilding site Wed Apr  6 05:13:22 PM CST 2022

--- searchurl/textarea.js
+++ searchurl/textarea.js
@@ -77,7 +77,10 @@ function moveDown(ta){
   ta.focus();
 }
 
-
-function onSave(fn){
+function save(fn){
   let u8=new TextEncoder().encode(textarea.value);let r='';for(let i=0;i<u8.byteLength;i++)r+=String.fromCharCode(u8[i]);location.href='i:0l'+fn+':'+btoa(r);
+}
+
+function onSave(){
+  save(location.pathname);
 }
