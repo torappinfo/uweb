8199f16d10596810236e5944aae1b2a05fa3e687
James Feng Cao <jamesfengcao@torapp.info>
2024-03-17 13:42:27 +0800

    fix deno.js to deploy static site

--- js/deno.js
+++ js/deno.js
@@ -2,10 +2,10 @@ import { serveFile } from "https://deno.land/std/http/file_server.ts";
 async function handler(req){
   let url = req.url;
   let iSlash = url.indexOf('/',11);
-  let path = url.substring(iSlash+1);
+  let path = url.substring(iSlash);
   if(path.endsWith('/'))
     path = path + 'index.html';
-  return await serveFile(req, `${Deno.cwd()}/`+path);
+  return await serveFile(req, `${Deno.cwd()}`+path);
 };
 
 Deno.serve(handler);
