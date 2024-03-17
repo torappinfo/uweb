9f8edef028c9ea2009fee2a2849c3f1003855a88
James Feng Cao <jamesfengcao@torapp.info>
2024-03-17 12:23:44 +0800

    deno.js to deploy static site

---
+++ js/deno.js
@@ -0,0 +1,9 @@
+import { serveFile } from "https://deno.land/std/http/file_server.ts";
+async function handler(req){
+  let url = req.url;
+  let iSlash = url.indexOf('/',11);
+  let path = url.substring(iSlash+1);
+  return await serveFile(req, `${Deno.cwd()}/`+path);
+};
+
+Deno.serve(handler);
