9d005633c8d9a43857eccaf306eea676fdb4b5e7
James Feng Cao <jamesfengcao@torapp.info>
2024-07-01 19:54:24 +0800

    uweb 1.0.49 to use s2ta.js instead of hash2textarea.js

--- misc/ebrowser/webview.js
+++ misc/ebrowser/webview.js
@@ -681,27 +681,3 @@ function translate(str){
   if(translateRes && (result=translateRes[str])) return result;
   return str;
 }
-
-async function jsonAppend(filePath, charcode, str){
-  try{
-    const fd = await fs.promises.open(filePath, 'r+');
-    const stats = await fd.stat();
-    const fileSize = stats.size;
-    const buffer = Buffer.alloc(1);
-    let position = fileSize-1;
-    while (position >= 0) {
-      await fd.read(buffer, 0, 1, position);
-      if (buffer[0] === charcode) break;
-    }
-    let endS = String.fromCharCode(charcode);
-    if(position<0){//re-write whole file
-      str = String.fromCharCode(charcode-2)+str+endS;
-      position = 0;
-    }else
-      str = ","+str+endS;
-    await fd.truncate(position);
-    const buf = Buffer.from(str);
-    await fd.write(buf, 0, buf.length, position);
-    await fd.close();
-  }catch(e){console.log(e)}
-}
